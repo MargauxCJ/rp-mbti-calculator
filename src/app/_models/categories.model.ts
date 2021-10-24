9f236e159f9c89963f71afc604e1d84df05613f9
MargauxCJ <mcjblanchet@yahoo.com>
2021-10-24 15:15:36 +0200

    Finish eneagrammeCalculatorService

--- src/app/_models/categories.model.ts
+++ src/app/_models/categories.model.ts
@@ -4,5 +4,6 @@ export interface Category {
     id: number;
     name: string;
     skills: Skill[];
+    instinct: string;
 }
 
--- src/app/_models/types.model.ts
+++ src/app/_models/types.model.ts
@@ -7,4 +7,5 @@ export interface EneType {
     desint: number;
     core: Skill;
     wingSkills: Skill[];
+    instinct: string;
 }
